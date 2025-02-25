2dd262d070679d74178d52dcbe061c44f0231385
Jonathan Vinicius <141459667+Jonathanfullstack@users.noreply.github.com>
2025-02-24 21:16:11 -0300

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,22 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Meu nome é Jonathan, tenho 21 anos e atualmente estou no 1º período do curso de Análise e Desenvolvimento de Sistemas (ADS). Estou muito empolgado com essa nova fase da minha vida acadêmica e tenho me dedicado bastante aos estudos, buscando sempre aprender mais sobre a área de tecnologia.\n" +
-                "\n" +
-                "Desde que comecei a faculdade, venho me apaixonando ainda mais pelas aulas e pelos desafios que a área de TI oferece. O conteúdo tem sido bastante interessante, e estou aprendendo muitas coisas novas, especialmente sobre programação, lógica de programação e desenvolvimento de sistemas. A cada dia, fico mais animado com o que está por vir e com as possibilidades que essa profissão pode me oferecer.\n" +
-                "\n" +
-                "Eu realmente gosto das aulas, porque sinto que estão me preparando para um futuro promissor no mercado de tecnologia. Estou sempre em busca de evoluir e expandir meus conhecimentos, e, com certeza, vou me empenhar ao máximo durante o curso para alcançar meus objetivos.");
+        // Apresentação pessoal
+        System.out.println("Meu nome é Jonathan, tenho 21 anos e atualmente estou no 1º período do curso de Análise e Desenvolvimento de Sistemas (ADS).");
+
+        // Empolgação com a nova fase acadêmica
+        System.out.println("Estou muito empolgado com essa nova fase da minha vida acadêmica e tenho me dedicado bastante aos estudos, buscando sempre aprender mais sobre a área de tecnologia.");
+
+        // Interesse pela área de TI e desenvolvimento de sistemas
+        System.out.println("\nDesde que comecei a faculdade, venho me apaixonando ainda mais pelas aulas e pelos desafios que a área de TI oferece.");
+        System.out.println("O conteúdo tem sido bastante interessante, e estou aprendendo muitas coisas novas, especialmente sobre programação, lógica de programação e desenvolvimento de sistemas.");
+
+        // Expectativas e motivação futura
+        System.out.println("\nA cada dia, fico mais animado com o que está por vir e com as possibilidades que essa profissão pode me oferecer.");
+
+        // Entusiasmo pelas aulas e preparação para o futuro
+        System.out.println("\nEu realmente gosto das aulas, porque sinto que estão me preparando para um futuro promissor no mercado de tecnologia.");
+        
+        // Compromisso com os estudos e objetivos
+        System.out.println("Estou sempre em busca de evoluir e expandir meus conhecimentos, e, com certeza, vou me empenhar ao máximo durante o curso para alcançar meus objetivos.");
     }
 }
